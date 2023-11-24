ade15791a9695ea52ae3ea3e68097088e8b84fc1
yurakovbuz1 <yura.kovbuz1@gmail.com>
2023-11-24 14:08:37 +0100

    finished

---
+++ js/task-3.js
@@ -0,0 +1,10 @@
+function getElementWidth(content, padding, border) {
+    contentWidth = parseFloat(content);
+    paddingWidth = parseFloat(padding);
+    borderWidth = parseFloat(border);
+    return contentWidth + paddingWidth*2 + borderWidth*2;
+}
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
